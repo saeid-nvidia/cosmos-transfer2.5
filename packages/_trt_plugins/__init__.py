776352362088ab3e69536b5cd48976b25272bd51
Krzysztof Tomala <ktomala@nvidia.com>
2026-02-26 15:12:46 +0100

    Revert "Merge pull request #8 from xrq-phys/patch-1"
    This reverts commit bb54a5c9951b45aa7dfc40d4e5d2911f93a0dc3e, reversing
    changes made to 044f38d0d2c230e0fbc7a4064f660f3cbd30835d.

--- packages/_trt_plugins/__init__.py
+++ packages/_trt_plugins/__init__.py
@@ -125,14 +125,9 @@ try:
 
         with torch.cuda.stream(ext_stream):
             # Prepare Op
-            if q_t.flatten(0, 1).shape[0] <= 7200:
-                # Sequence is short. Use SDPA->cuDNN
-                op = get_a2a_op(cp_group72, backend="torch")
-            elif get_sm_version(q_t.device.index) // 10 != 10:
-                # Use OSS SageAttention for Ada, Hopper, and RTX Blackwell
-                op = get_a2a_op(cp_group72, backend="sageattn")
+            if get_sm_version(q_t.device.index) // 10 != 10:
+                op = get_a2a_op(cp_group72)
             else:
-                # Use TRTLLM-Gen kernels for B200 & GB200
                 op = get_a2a_op(cp_group72, backend="flashinfer_vx")
 
             # Execute
